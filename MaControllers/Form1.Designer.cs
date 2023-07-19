c0e096189958ce582832d86e7c973c1186e4b28b
Marco Antonio <marcoantonigoncas@gmail.com>
2023-07-19 00:37:27 -0600

    Modificado README.md

--- MaControllers/Form1.Designer.cs
+++ MaControllers/Form1.Designer.cs
@@ -29,41 +29,73 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButtonCustom1 = new MaControllers.Controles.RadioButtonCustom();
             this.trackBarCustom2 = new MaControllers.Controles.TrackBarControls.TrackBarCustom();
             this.buttonCustom1 = new MaControllers.Controles.ButtonCustom();
             this.circularPictureBox1 = new MaControllers.Controles.CircularPictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // label1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.checkBox1.Location = new System.Drawing.Point(27, 12);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 11;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 22);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Button Custom:";
             // 
-            // trackBar1
+            // label2
             // 
-            this.trackBar1.Location = new System.Drawing.Point(303, 82);
-            this.trackBar1.Maximum = 2000;
-            this.trackBar1.Minimum = 2000;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.trackBar1.Size = new System.Drawing.Size(45, 151);
-            this.trackBar1.TabIndex = 13;
-            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.trackBar1.Value = 2000;
-            this.trackBar1.MouseCaptureChanged += new System.EventHandler(this.trackBar1_MouseCaptureChanged);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 22);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "PictureBox Custom:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(270, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 22);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "RadioButton Custom:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(270, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 22);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Trackbar Custom:";
+            // 
+            // radioButtonCustom1
+            // 
+            this.radioButtonCustom1.AutoSize = true;
+            this.radioButtonCustom1.BallColorActive = System.Drawing.Color.DarkCyan;
+            this.radioButtonCustom1.BorderColor = System.Drawing.Color.Crimson;
+            this.radioButtonCustom1.BorderColorActive = System.Drawing.Color.RoyalBlue;
+            this.radioButtonCustom1.Location = new System.Drawing.Point(274, 239);
+            this.radioButtonCustom1.MinimumSize = new System.Drawing.Size(0, 21);
+            this.radioButtonCustom1.Name = "radioButtonCustom1";
+            this.radioButtonCustom1.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.radioButtonCustom1.Size = new System.Drawing.Size(135, 21);
+            this.radioButtonCustom1.TabIndex = 16;
+            this.radioButtonCustom1.TabStop = true;
+            this.radioButtonCustom1.Text = "radioButtonCustom1";
+            this.radioButtonCustom1.UseVisualStyleBackColor = true;
             // 
             // trackBarCustom2
             // 
@@ -73,17 +105,17 @@
             this.trackBarCustom2.ColorThumbHover = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(99)))), ((int)(((byte)(102)))));
             this.trackBarCustom2.Font = new System.Drawing.Font("Berlin Sans FB", 11.25F, System.Drawing.FontStyle.Italic);
             this.trackBarCustom2.ForeColorTooltip = System.Drawing.SystemColors.ControlLightLight;
-            this.trackBarCustom2.Location = new System.Drawing.Point(249, 304);
+            this.trackBarCustom2.Location = new System.Drawing.Point(250, 65);
             this.trackBarCustom2.MaxValue = 100;
             this.trackBarCustom2.MinimumSize = new System.Drawing.Size(100, 60);
             this.trackBarCustom2.MinValue = 0;
             this.trackBarCustom2.Name = "trackBarCustom2";
             this.trackBarCustom2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             this.trackBarCustom2.ShowTooltip = true;
-            this.trackBarCustom2.ShowValues = false;
-            this.trackBarCustom2.Size = new System.Drawing.Size(181, 60);
+            this.trackBarCustom2.ShowValues = true;
+            this.trackBarCustom2.Size = new System.Drawing.Size(181, 81);
             this.trackBarCustom2.SizeRadiusPercent = 100;
-            this.trackBarCustom2.SizeThumb = 15;
+            this.trackBarCustom2.SizeThumb = 20;
             this.trackBarCustom2.TabIndex = 15;
             this.trackBarCustom2.Text = "trackBarCustom2";
             this.trackBarCustom2.TrackColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(158)))), ((int)(((byte)(158)))));
@@ -107,7 +139,7 @@
             this.buttonCustom1.GradientBorder = true;
             this.buttonCustom1.GradientBorderAngle = 45;
             this.buttonCustom1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonCustom1.Location = new System.Drawing.Point(39, 61);
+            this.buttonCustom1.Location = new System.Drawing.Point(39, 65);
             this.buttonCustom1.Name = "buttonCustom1";
             this.buttonCustom1.Size = new System.Drawing.Size(177, 43);
             this.buttonCustom1.TabIndex = 10;
@@ -124,7 +156,7 @@
             this.circularPictureBox1.BorderSize = 0;
             this.circularPictureBox1.CapStyle = System.Drawing.Drawing2D.DashCap.Round;
             this.circularPictureBox1.Image = global::MaControllers.Properties.Resources._34;
-            this.circularPictureBox1.Location = new System.Drawing.Point(39, 122);
+            this.circularPictureBox1.Location = new System.Drawing.Point(39, 219);
             this.circularPictureBox1.MinimumSize = new System.Drawing.Size(20, 20);
             this.circularPictureBox1.Name = "circularPictureBox1";
             this.circularPictureBox1.Size = new System.Drawing.Size(177, 177);
@@ -138,15 +170,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonCustom1);
             this.Controls.Add(this.trackBarCustom2);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.buttonCustom1);
             this.Controls.Add(this.circularPictureBox1);
             this.Name = "Form1";
             this.Text = " ";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -156,10 +190,13 @@
         #endregion
         private Controles.CircularPictureBox circularPictureBox1;
         private Controles.ButtonCustom buttonCustom1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.ToolTip toolTip1;
         private Controles.TrackBarControls.TrackBarCustom trackBarCustom2;
+        private Controles.RadioButtonCustom radioButtonCustom1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# MaControllers
+
+<h1 align="center">Controles personalizados </h1>
+
+## ¿Que es?
+Son controles extra de windows forms que permiten ajustes adicionales.
+
+
+## Incluye
+- Button custom
+- Picturebux custom
+- RadioButton custom
+- Tackbar/Slider custom 
+
+
+## Button custom 
+
+![Imagen de controles](/assets/img/Controls.png)
+![Imagen de controles activos](/assets/img/Controls.png)
+
+Contribucion a [RJControls](https://github.com/RJCodeAdvance/RJControls)
